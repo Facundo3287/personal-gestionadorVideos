561680e540c38b908268c4f00aabed111c87650a
Facundo Rodriguez <fr8080342@gmail.com>
2026-02-06 12:24:17 -0300

    server & vite

---
+++ src-api/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+let app = express();
+let port: number = 8000;
+let url: string = import.meta.url;
+let __dirname: string = fileURLToPath(url);
+let ruta: string = path.join(__dirname, '..', '..', 'dist');
+app.use(express.static(ruta));
+
+app.listen(port, () => { console.log(`server prendido en el puerto ${port}...`) } )
